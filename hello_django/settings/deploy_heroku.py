e1f9b5552b7ae2c1d041caae6671dca9cfe2f0b8
Eddy <eddy15201@gmail.com>
2021-07-10 20:39:01 +0800

    update

--- hello_django/settings/deploy_heroku.py
+++ hello_django/settings/deploy_heroku.py
@@ -11,7 +11,8 @@ SECRET_KEY = get_env_var('DJANGO_SECRET_KEY')
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # 設定靜態檔位置。
-STATIC_ROOT = 'static'
+# STATIC_ROOT = 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # 設定資料庫。
 DATABASES = {
